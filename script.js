2d9fbcf53204f53d64722ec561238f61ec4ff24b
mkreive <mkreive@gmail.com>
2022-03-31 11:46:52 +0300

    validations

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const firstName = document.getElementById("firstName");
 const lastName = document.getElementById("lastName");
 const passwordEl = document.getElementById("password");
 const emailEl = document.getElementById("email");
-const address = document.getElementById("address");
+const addressEl = document.getElementById("address");
 const formField = document.querySelectorAll(".formField");
 
 // VALIDATIONS
@@ -26,83 +26,77 @@ const isPasswordSecure = (password) => {
     return re.test(password);
 };
 
-const checkUsername = () => {
+const checkUsername = (name) => {
     let valid = false;
     const min = 3,
         max = 25;
-    const username = firstName.value.trim();
+    const username = name.value.trim();
 
-    if (!isRequired(username)) {
-        console.log(firstName, "Username cannot be blank.");
-    } else if (!isBetween(username.length, min, max)) {
-        console.log(
-            firstName,
-            `Username must be between ${min} and ${max} characters.`
-        );
+    if (!isRequired(username) || !isBetween(username.length, min, max)) {
+        let message = "Smith";
+        showError(name, message);
     } else {
-        showSuccess(firstName);
+        showSuccess(name);
         valid = true;
     }
     return valid;
 };
-const checkEmail = () => {
+const checkEmail = (emailAddress) => {
     let valid = false;
-    const email = emailEl.value.trim();
-    if (!isRequired(email)) {
-        console.log(emailEl, "Email cannot be blank.");
-    } else if (!isEmailValid(email)) {
-        console.log(emailEl, "Email is not valid.");
+    const email = emailAddress.value.trim();
+    if (!isRequired(email) || !isEmailValid(email)) {
+        let message = "email@email.com";
+        showError(emailAddress, message);
     } else {
-        showSuccess(emailEl);
+        showSuccess(emailAddress);
         valid = true;
     }
     return valid;
 };
-const checkPassword = () => {
+const checkPassword = (passwordSubmitted) => {
     let valid = false;
-    const password = passwordEl.value.trim();
-    if (!isRequired(password)) {
-        showError(passwordEl, "Password cannot be blank.");
-    } else if (!isPasswordSecure(password)) {
-        console.log(
-            passwordEl,
-            "Password must has at least 8 characters that include at least 1 number"
-        );
+    const password = passwordSubmitted.value.trim();
+    if (!isRequired(password) || !isPasswordSecure(password)) {
+        let message =
+            "Must contain at least one number and be 8 characters long";
+        showError(passwordSubmitted, message);
     } else {
-        showSuccess(passwordEl);
+        showSuccess(passwordSubmitted);
         valid = true;
     }
-
     return valid;
 };
 
 // ERROR OR SUCESS CLASSES
 const showError = (input, message) => {
     const formField = input;
-
     formField.classList.remove("input__success");
     formField.classList.add("error");
-
-    formField.textContent = "wrong";
+    formField.textContent = message;
 };
 const showSuccess = (input) => {
-    const formField = input.parentElement;
-
+    const formField = input;
     formField.classList.remove("error");
     formField.classList.add("input__success");
-
-    input.textContent = "";
+    input.value = "";
 };
 
 // LISTENER
 form.addEventListener("submit", function (e) {
     e.preventDefault();
 
-    let isUsernameValid = checkUsername(),
-        isEmailValid = checkEmail(),
-        isPasswordValid = checkPassword();
+    let isFirstNameValid = checkUsername(firstName),
+        isLastNameValid = checkUsername(lastName),
+        isEmailValid = checkEmail(emailEl),
+        isPasswordValid = checkPassword(passwordEl),
+        isAddressValid = checkUsername(addressEl);
 
-    let isFormValid = isUsernameValid && isEmailValid && isPasswordValid;
+    let isFormValid =
+        isFirstNameValid &&
+        isLastNameValid &&
+        isEmailValid &&
+        isPasswordValid &&
+        isAddressValid;
 
     if (isFormValid) {
         console.log("form is valid");
